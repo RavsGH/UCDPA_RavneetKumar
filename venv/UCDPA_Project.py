c83479716db2bbd6ef1c88748ba569ac9d7b062a
RavsGH <sonieducation6@gmail.com>
2021-12-13 14:38:34 +0530

    new file UCDPA_Project.py checked in

--- venv/UCDPA_Project.py
+++ venv/UCDPA_Project.py
@@ -1,6 +1,11 @@
+# Import all the required libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.modelselection import train_test_split
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
+
+# Import the both the datasets (Calories.csv and Excercise.csv)
+calories_burnt=pd.read_csv('calories.csv')
